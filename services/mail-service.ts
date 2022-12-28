9dc35248bea064fced08e33e6143a37d2f533d09
Artem <karpuxov.a@mail.ru>
2022-12-29 02:39:09 +0300

    new smtp service

--- services/mail-service.ts
+++ services/mail-service.ts
@@ -1,18 +1,15 @@
 import nodemailer from "nodemailer";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env;
-console.log(SMTP_HOST, 'SMTP_HOST');
-console.log(SMTP_PASSWORD, 'SMTP_PASSWORD');
+
 class MailService {
   transporter: nodemailer.Transporter;
   constructor() {
     this.transporter = nodemailer.createTransport({
-      //@ts-ignore
       host: SMTP_HOST,
-      port: SMTP_PORT,
-      secure: true,
+      port: +SMTP_PORT,
       auth: {
         user: SMTP_USER,
         pass: SMTP_PASSWORD,
@@ -39,16 +36,16 @@ class MailService {
     await this.transporter.sendMail({
       from: process.env.SMTP_USER,
       to,
-      subject: 'Актицвация аккаунта',
-      text: '',
+      subject: "Актицвация аккаунта",
+      text: "",
       html: `
         <div>
           <h1>Для смены пароля перейдите по ссылке</h1>
           <a href="${link}">${link}</a>
         </div>
-      `
-    })
-  }
+      `,
+    });
+  };
 }
 
 export default new MailService();

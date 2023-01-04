3287db985f64dddb0e32f8dd2e849ea1750631e9
Artem <karpuxov.a@mail.ru>
2023-01-04 16:09:25 +0300

    cors cridentials

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);

81604a954aa815d205c83b9a826cacdbf8a86ec0
Artem <karpuxov.a@mail.ru>
2023-01-04 13:33:02 +0300

    cors

--- index.ts
+++ index.ts
@@ -11,17 +11,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-  //allow access to current url. work for https as well
-  res.setHeader("Access-Control-Allow-Origin", req.header("Origin") as string);
-  res.removeHeader("x-powered-by");
-  //allow access to current method
-  res.setHeader("Access-Control-Allow-Methods", req.method);
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
 app.use(express.json());
-// app.use(cors());
 app.use(cookieParser());
 app.use("/api", router);
 app.use(errorMiddleware);

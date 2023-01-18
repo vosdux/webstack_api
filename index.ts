8727adcecad990fec37e4198fdda9f9d07ae7498
Artem <karpuxov.a@mail.ru>
2023-01-18 16:28:54 +0300

    cookies fix

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({ credentials: true, origin: true, allowedHeaders: ['authorization', 'content-type', 'Cookie'] }));
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
